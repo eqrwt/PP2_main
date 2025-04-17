9a626057bd117e4ba9c9c7bf1a8dabb1ab863b1c
Aliman Bibolov <jorgefibonacci413@gmail.com>
2025-04-17 13:41:47 +0500

    commiting Lab 11 with python files

---
+++ Lab11/main.py
@@ -0,0 +1,88 @@
+import psycopg2
+from config import load_config
+
+#execution sql
+def execute_sql_file(filename):
+    params = load_config()
+    with open(filename, 'r') as file:
+        sql = file.read()
+
+    try:
+        with psycopg2.connect(**params) as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql)
+            conn.commit()
+        print("SQL file executed successfully.")
+    except Exception as e:
+        print("Error executing SQL file:", e)
+
+# searching
+def search_phonebook(keyword):
+    params = load_config()
+    try:
+        with psycopg2.connect(**params) as conn:
+            with conn.cursor() as cur:
+                cur.callproc('search_phonebook', (keyword, ))
+
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
+                
+            conn.commit()
+    except (psycopg2.DatabaseError, Exception) as error:
+        print("!!ERROR: !!", error)
+
+
+#quering 
+def printing():
+    params = load_config()
+    try:
+        with psycopg2.connect(**params) as conn:
+            with conn.cursor() as cur:
+                cur.callproc("get_phonebook_paginated", (5, 0))
+
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
+                
+            conn.commit()
+    except (psycopg2.DatabaseError, Exception) as error:
+        print("!!ERROR: !!", error)
+
+def delete(value):
+    params = load_config()
+    try:
+        with psycopg2.connect(**params) as conn:
+            with conn.cursor() as cur:
+                cur.execute('CALL delete_user(%s, %s)', (value, value))
+
+            conn.commit()
+    except (psycopg2.DatabaseError, Exception) as error:
+        print("!!ERROR: !!", error)
+    finally:
+        print(f"Deleted: {value}")
+    
+def insert_input():
+    params = load_config()
+
+    username = input("Enter Username: ")
+    phone = input("Enter Phone: ")
+    try:
+        with psycopg2.connect(**params) as conn:
+            with conn.cursor() as cur:
+
+                cur.execute("CALL insert_or_update_user(%s, %s)", (username, phone))
+        
+        conn.commit()
+    except (psycopg2.DatabaseError, Exception) as error:
+        print("!!ERROR: !!", error)
+
+execute_sql_file("phonebook_db_logic.sql")
+printing()
+
+printing()
+
+
+
+
+
--- Lab11/phonebook_db_logic.sql
+++ Lab11/phonebook_db_logic.sql
@@ -1,56 +1,19 @@
 -- Search by part of name, surname or phone
+DROP FUNCTION IF EXISTS search_phonebook(TEXT);
+
 CREATE OR REPLACE FUNCTION search_phonebook(pattern TEXT)
-RETURNS TABLE(id INT, name TEXT, surname TEXT, phone TEXT) AS $$
+RETURNS TABLE(id INT, username VARCHAR, phone VARCHAR) AS $$
 BEGIN
     RETURN QUERY 
     SELECT * FROM phonebook
-    WHERE name ILIKE '%' || pattern || '%' 
-       OR surname ILIKE '%' || pattern || '%'
-       OR phone ILIKE '%' || pattern || '%';
+    WHERE phonebook.username ILIKE '%' || pattern || '%'
+       OR phonebook.phone ILIKE '%' || pattern || '%';
 END;
 $$ LANGUAGE plpgsql;
-
--- PROCEDURE to insert a new user or update if exists
-CREATE OR REPLACE PROCEDURE insert_or_update_user(username TEXT, userphone TEXT)
-AS $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM phonebook WHERE name = username) THEN
-        UPDATE phonebook SET phone = userphone WHERE name = username;
-    ELSE
-        INSERT INTO phonebook(name, phone) VALUES (username, userphone);
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
--- Insert many users
-CREATE OR REPLACE PROCEDURE insert_many_users(users JSONB, OUT invalid_data JSONB)
-LANGUAGE plpgsql
-AS $$
-DECLARE
-    item JSONB;
-    uname TEXT;
-    uphone TEXT;
-    errors JSONB := '[]'::JSONB;
-BEGIN
-    FOR item IN SELECT * FROM jsonb_array_elements(users)
-    LOOP
-        uname := item->>'name';
-        uphone := item->>'phone';
-
-        -- Simple phone check (e.g., 10 digits)
-        IF uphone ~ '^\d{10}$' THEN
-            CALL insert_or_update_user(uname, uphone);
-        ELSE
-            errors := errors || jsonb_build_object('name', uname, 'phone', uphone);
-        END IF;
-    END LOOP;
-    invalid_data := errors;
-END;
-$$;
 
 -- FUNCTION to query with pagination
 CREATE OR REPLACE FUNCTION get_phonebook_paginated(limit_size INT, offset_count INT)
-RETURNS TABLE(id INT, name TEXT, phone TEXT)
+RETURNS TABLE(id INT, username VARCHAR, phone VARCHAR)
 AS $$
 BEGIN
     RETURN QUERY
@@ -66,9 +29,25 @@ LANGUAGE plpgsql
 AS $$
 BEGIN
     IF u_name IS NOT NULL THEN
-        DELETE FROM phonebook WHERE name = u_name;
+        DELETE FROM phonebook WHERE phonebook.username = u_name;
     ELSIF u_phone IS NOT NULL THEN
         DELETE FROM phonebook WHERE phone = u_phone;
     END IF;
 END;
 $$;
+
+-- PROCEDURE to insert a new user or update if exists
+DROP PROCEDURE insert_or_update_user(TEXT,TEXT);
+
+CREATE OR REPLACE PROCEDURE insert_or_update_user(new_username TEXT, userphone TEXT)
+AS $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM phonebook WHERE phonebook.username = new_username) THEN
+        UPDATE phonebook SET phone = userphone WHERE phonebook.username = new_username;
+    ELSE
+        INSERT INTO phonebook(username, phone) VALUES (new_username, userphone);
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
